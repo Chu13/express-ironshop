25ca33c8bc6cf1f769aedf723e5047dd31fda7e8
Jesus Bordones <jabordones92@gmail.com>
2017-11-13 14:03:52 -0500

    new repo

--- models/product-model.js
+++ models/product-model.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema ({
-  // fields here
+  name: { type: String },
+  price: { type: Number },
+
+  imageUrl: { type: String },
+  description: { type: String },
+  dateAdded: { type: Date },
 });
 
 const ProductModel = mongoose.model("Product", productSchema);
